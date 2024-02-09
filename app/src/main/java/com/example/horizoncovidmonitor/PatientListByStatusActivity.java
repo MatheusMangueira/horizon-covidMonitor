55396c9a7476cc30daeb7462d99aeb81d4c6e6de
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-02-09 16:25:06 -0300

    feat: create PatientListByStatusActivity

---
+++ app/src/main/java/com/example/horizoncovidmonitor/PatientListByStatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.horizoncovidmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientListByStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_list_by_status);
+    }
+}
