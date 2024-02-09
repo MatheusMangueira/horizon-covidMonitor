d35d4c8735808f238c85f9813548000b942c280b
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-02-09 04:14:24 -0300

    fix: border and back button on patient list page

--- app/src/main/java/com/example/horizoncovidmonitor/PatientListActivity.java
+++ app/src/main/java/com/example/horizoncovidmonitor/PatientListActivity.java
@@ -1,6 +1,9 @@
 package com.example.horizoncovidmonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,7 @@ import java.util.List;
 
 public class PatientListActivity extends AppCompatActivity {
     private ListView listView;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,15 @@ public class PatientListActivity extends AppCompatActivity {
         PatientListAdapter adapter = new PatientListAdapter(this, patients);
 
         listView.setAdapter(adapter);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PatientListActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
